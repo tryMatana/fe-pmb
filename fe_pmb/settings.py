9680b0e9e33fcc6695f67e082d965490c6bd6367
tryMatana <tri.ariyanto@matanauniversity.ac.id>
2025-07-11 16:28:38 +0700

    add alowed host

--- fe_pmb/settings.py
+++ fe_pmb/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "your-secret-key-here"
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tryMatana.pythonanywhere.com']
 
 INSTALLED_APPS = [
     "django.contrib.admin",
